cdcb9e518d4c0081c642ea3d54bc14d4b5c59987
Guilherme Soares <soaresgui.dev@gmail.com>
2025-03-19 16:19:17 -0400

    update: solving package conflicts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   images: {
-    domains: ["github.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/guisoaresdev/**",
+        search: "",
+      },
+    ],
   },
 };
