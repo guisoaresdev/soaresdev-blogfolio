d15b9d73867bd74145b0d4a41c1277fd7990acc9
Guilherme Soares <soaresgui.dev@gmail.com>
2025-03-26 01:23:53 -0400

    fix(project): Removing All unnexistant imports HOPEFULLY

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,20 +1,14 @@
-import { CMS_NAME } from "@/lib/constants";
-
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        SoaresDev Blog/Portfolio.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+        <a className="underline hover:text-blue-600 duration-200 transition-colors">
+          Eu sou Guilherme Soares, um desenvolvedor full stack e esse é meu blog
+          e portfolio.
+        </a>
       </h4>
     </section>
   );
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getAllProjects } from "@/lib/api";
-import { CMS_NAME } from "@/lib/constants";
 import markdownToHtml from "@/lib/markdownToHtml";
 import Container from "@/app/_components/container";
 import { MoreProjects } from "@/app/_components/more-projects";
